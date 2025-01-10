7b8cba2613a3bac49ccd463d32757835a7a16326
BT_1N3_32 <sample@bths.edu>
2025-01-10 10:45:54 -0500

    first person commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Who are you");
-        System.out.println("hi");
+        System.out.println("bot");
         System.out.println("hello");
     }
 }
